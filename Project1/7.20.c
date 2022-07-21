e3d7dacbfe87acaa317b95ca141589eb1ec5d760
fengguocheng <1375581920@qq.com>
2022-07-21 17:52:22 +0800

    更新代码

--- Project1/7.20.c
+++ Project1/7.20.c
@@ -40,10 +40,46 @@
 //}
 #include <stdio.h>
 #include <math.h>
-int main()
-{
-	int a = 1, b = 1;
-	int c = a > b ? 2 : (a < b ? 1 : 0);
+//void Addself()
+//{
+//	static int tmp = 0;
+//	tmp += 1;
+//	printf("%d", tmp);
+//}
+//int main()
+//{
+//	
+//	/*extern num;
+//	int main()
+//	{
+//		printf("%d", num);
+//		return 0;
+//	}
+//	for (int i = 1; i <= 2; i++)
+//	{
+//		Addself();
+//	}
+//	return 0;*/
+//}
+//int main()
+//{
+//	int a = 1, b = 1;
+//	int c = a > b ? 2 : (a < b ? 1 : 0);
+//	switch (c)
+//	{
+//	case 2:
+//		printf("%d>%d\n", a, b);
+//		break;
+//	case 1:
+//		printf("%d<%d\n", a, b);
+//		break;
+//	case 0:
+//		printf("%d=%d\n", a, b);
+//		break;
+//	}
+//	return 0;
+//}
+	
 	//int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	//int max = a[0];
 	//for (int i = 1; i < 10; i++)
@@ -61,7 +97,7 @@ int main()
 		}
 		printf("\n");
 	}*/
-	return 0;
+	//return 0;
 	/*int a, b, c;
 	
 	while (1)
@@ -82,7 +118,7 @@ int main()
 		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 			printf("%d ", year);
 	}*/
-	return 0;
+	//return 0;
 	/*int i, j;
 	for (i = 101; i <= 200; i+=2)
 	{
@@ -95,7 +131,7 @@ int main()
 			printf("%d ", i);
 	}
 	return 0;*/
-}
+
 	/*int a[3];
 	int i, j, tmp;
 	for (i = 0; i < 3; i++)
@@ -163,3 +199,17 @@ int main()
 //	printf("%d", b);
 //	return b;
 //}
+int main()
+{
+		//整型数a，字符c，浮点数f，字符串str（可看做字符串）
+		int a;
+		char c;
+		float f;
+		char str[20];
+		/*scanf("%d", &a);
+		scanf("%c", &c);
+		scanf("%f", &f);
+		scanf("%s", &str);*/
+		scanf("%d,%c,%f,%s", &a, &c, &f, &str);
+		printf("a=%d,c=%c,f=%f,str=%s\n", a, c, f, str);
+}
