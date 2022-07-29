eb7fc3d1b14a34b7dd7ae337beff2174b99f66a3
fengguocheng <1375581920@qq.com>
2022-07-29 16:17:02 +0800

    提交代码

---
+++ 7.29/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	/*int main()
+	{
+		int m, n;
+		int count = 0;
+		scanf("%d %d", &m, &n);
+		int c = m ^ n;
+		while (c)
+		{
+			count++;
+			c = c & (c - 1);
+		}
+		printf("%d ", count);
+		return 0;
+	}*/
+	/*int input = -15;
+	int i;
+	int count = 0;
+	for (i = 0; i < 32; i++)
+	{
+		if ((input >> i) & 1)
+			count++;
+	}
+	printf("%d", count);*/
+	/*int m, n;
+	int count = 0;
+	scanf("%d %d", &m, &n);
+	int c = m ^ n;
+	while (c)
+	{
+		count++;
+		c = c & (c - 1);
+	}
+	printf("%d ", count);*/
+	//int m = 20;
+	//int n = 10;
+	//m = m + n;//m=m^n
+	//n = m - n;//n=m^n
+	//m = m - n;//m=m^n
+	//printf("%d %d ", m, n);
+	return 0;
+}
